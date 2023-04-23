8505d93d16918ef1c4f56dda650ed5a210754e34
Paulo H. C. Jesus <paulohenriquecardosodjesus@gmail.com>
2023-04-23 13:00:13 -0300

    calculadora funções de potencia e demais eventos de click

--- src/components/Calculadora/index.js
+++ src/components/Calculadora/index.js
@@ -1,45 +1,145 @@
 import React, { useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
-import Botao from '../Botao/index'
-import Ionicons from '@expo/vector-icons/Ionicons';
-
+import Botao from "../Botao/index";
+import Ionicons from "@expo/vector-icons/Ionicons";
 
 const index = () => {
+  const [display, setDisplay] = useState("0");
+  const [display2, setDisplay2] = useState(null);
+  const [value, setValue] = useState(null);
+  const [number, setNumber] = useState(null);
+  const [operator, setOperator] = useState(null);
+  var total = 0.0;
   const operationButtons = ["+", "-", "*", "/", "="];
   const numberButtons = Array.from({ length: 10 }, (_, i) =>
     String.fromCharCode(i + 48)
   );
 
-  const [icalc, setIcalc] = useState(0);
+  const valueEvent = (number) => {
+    if (display === "0") {
+      setDisplay(number);
+    } else {
+      setDisplay(`${display}${number}`);
+    }
+    setNumber(parseFloat(number));
+  };
+
+  const operatorEvent = (oprt) => {
+    const inputDisplay = parseFloat(display);
+    if (oprt === "x²") {
+      total = calculateValue(inputDisplay, 2, oprt)
+      console.warn(total);
+      setValue(total);
+      setDisplay(total);
+      setDisplay2(`${display}${oprt}`)
+    }else if (value === null) {
+      const inputDisplay = parseFloat(display);
+      setValue(inputDisplay);
+      setDisplay(`${display}${oprt}`);
+      setOperator(oprt);
+    } else if (oprt === "=") {
+      const newValue = String(calculateValue(value, number, operator));
+      setDisplay(`${value} ${operator} ${number} = ${newValue}`);
+      total = parseFloat(newValue);
+    } else {
+      const newValue = calculateValue(value, number, oprt);
+      setValue(newValue);
+      setDisplay2(`${display} = ${newValue}`);
+      setDisplay(`${newValue}${oprt}`);
+    }
+  };
+
+  const calculateValue = (value1, value2, operator) => {
+    switch (operator) {
+      case "+":
+        return value1 + value2;
+      case "-":
+        return value1 - value2;
+      case "*":
+        return value1 * value2;
+      case "/":
+        return value1 / value2;
+      case "x²":
+        return value1 ** value2;
+      case "x^":
+        return value1 ** value2;
+      default:
+        return value2;
+    }
+  };
+
+  const btnClean = () => {
+    setDisplay("0");
+    setDisplay2(null);
+    setValue(null);
+    setNumber(null);
+  };
+  const btnBackSpace = () => {
+    setDisplay(display.slice(0, -1));
+  };
+
+  const equalsEvent = (value, number, oprt) => {};
 
   return (
     <View style={styles.container}>
       <View style={styles.contentBoxView}>
-        <Text style={styles.boxView}></Text>
-        <Text style={styles.boxView}>{icalc}</Text>
+        <Text style={styles.boxView}>{display2}</Text>
+        <Text style={styles.boxView}>{display}</Text>
       </View>
       <View style={styles.contentBoxButton}>
-
         <View style={styles.numBoxButton}>
-          <Botao element={"C"}></Botao>
-          <Botao key={","} element={","} onPress={() => { setIcalc(',') }}></Botao>
-          {numberButtons.map((element) =>
-            (<Botao key={element} element={element} onPress={() => { setIcalc(numberButtons[element]) }} />))
-          }
-          <Botao element={"X²"} />
-          <Botao element={"X³"} />
-          <TouchableOpacity style={{alignItems:'center',justifyContent:'center', margin:8}}>
-          <Ionicons name="backspace-outline"
-            size={50} color="black"
+          <Botao key={"C"} element={"C"} onPress={btnClean} />
+          <Botao
+            key={","}
+            element={","}
+            onPress={() => {
+              valueEvent(",");
+            }}
           />
+          {numberButtons.map((element) => (
+            <Botao
+              key={element}
+              element={element}
+              onPress={() => {
+                valueEvent(element);
+              }}
+            />
+          ))}
+
+          <Botao
+            element={"X²"}
+            onPress={() => {
+              operatorEvent("x²");
+            }}
+          />
+          <Botao
+            element={"X^"}
+            onPress={() => {
+              operatorEvent("x^");
+            }}
+          />
+
+          <TouchableOpacity
+            style={{
+              alignItems: "center",
+              justifyContent: "center",
+              margin: 8,
+            }}
+            onPress={btnBackSpace}
+          >
+            <Ionicons name="backspace-outline" size={50} color="black" />
           </TouchableOpacity>
         </View>
 
         <View style={styles.operationBoxButton}>
           {operationButtons.map((element) => (
-            <Botao key={element} element={element} onPress={() => {
-              setIcalc(element)
-            }} />
+            <Botao
+              key={element}
+              element={element}
+              onPress={() => {
+                operatorEvent(element);
+              }}
+            />
           ))}
         </View>
       </View>
@@ -49,39 +149,35 @@ const index = () => {
 
 const styles = StyleSheet.create({
   container: {
-    height: '90.3%',
+    height: "90.3%",
     alignItems: "center",
-    borderWidth: 1,
     padding: 5,
-    backgroundColor: "#c55",
-    justifyContent: 'space-around',
+    justifyContent: "space-around",
   },
   contentBoxView: {
-    backgroundColor: "#ccc",
+    borderWidth: 1,
     flex: 0.3,
-    width: '100%',
+    width: "100%",
   },
   boxView: {
-    height: '50%',
-    textAlign: 'right',
-    textAlignVertical: 'bottom',
+    height: "50%",
+    textAlign: "right",
+    textAlignVertical: "bottom",
     fontSize: 44,
     paddingRight: 20,
     paddingBottom: 20,
   },
   contentBoxButton: {
-    backgroundColor: "#ccc",
-    borderWidth: 1,
     flex: 0.65,
-    width: '100%',
-    alignItems: 'center',
-    justifyContent: 'center',
-    flexDirection: 'row',
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
   },
   numBoxButton: {
     width: "57.5%",
-    flexDirection: 'row',
-    flexWrap: 'wrap-reverse',
+    flexDirection: "row",
+    flexWrap: "wrap-reverse",
   },
   operationBoxButton: {
     width: "20%",
